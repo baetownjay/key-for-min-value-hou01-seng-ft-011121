665331c5bc8f1c232bf554a73bdef56b8155a3d8
Jason Herrera <baytownjason@yahoo.com>
2020-12-16 22:22:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,9 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-lowest_value = 2000
+
 def key_for_min_value(name_hash)
+  lowest_value = 2000
   name_hash.each do |key, value|
     if name_hash[:key] < lowest_value
       lowest_value = name_hash[:key]
