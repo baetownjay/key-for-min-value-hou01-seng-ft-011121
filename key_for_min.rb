a65ce8c560b3069f85d41b8c24d82476daa3e2ae
Jason Herrera <baytownjason@yahoo.com>
2020-12-16 22:43:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest_value = 2000
   key_value = ""
   name_hash.each do |item, price|
-    if [price] < lowest_value
+    if price.to_i < lowest_value
       lowest_value = price
       key_value = item
     end
